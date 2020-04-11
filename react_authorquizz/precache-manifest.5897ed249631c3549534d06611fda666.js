7406b6a0281a56afd43bf3be3c7e401a116b6861
Garri <cibergarri@hotmail.com>
2020-04-11 17:33:00 +0200

    fix: routes

--- react_authorquizz/precache-manifest.5897ed249631c3549534d06611fda666.js
+++ react_authorquizz/precache-manifest.5897ed249631c3549534d06611fda666.js
@@ -1,22 +1,22 @@
 self.__precacheManifest = [
   {
     "revision": "b69e58d19a137fdb084f",
-    "url": "/react_authorquizz/static/css/main.dc12abec.chunk.css"
+    "url": "/playground/react_authorquizz/static/css/main.dc12abec.chunk.css"
   },
   {
     "revision": "b69e58d19a137fdb084f",
-    "url": "/react_authorquizz/static/js/main.b69e58d1.chunk.js"
+    "url": "/playground/react_authorquizz/static/js/main.b69e58d1.chunk.js"
   },
   {
     "revision": "4cf2a3c2febd79979c5e",
-    "url": "/react_authorquizz/static/js/1.4cf2a3c2.chunk.js"
+    "url": "/playground/react_authorquizz/static/js/1.4cf2a3c2.chunk.js"
   },
   {
     "revision": "6b4de26d294e29baf4e0",
-    "url": "/react_authorquizz/static/js/runtime~main.6b4de26d.js"
+    "url": "/playground/react_authorquizz/static/js/runtime~main.6b4de26d.js"
   },
   {
     "revision": "b4b030609ad86aad7dde300495c90345",
-    "url": "/react_authorquizz/index.html"
+    "url": "/playground/react_authorquizz/index.html"
   }
 ];
