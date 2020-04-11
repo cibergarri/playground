0e25a573aba648bcc6c8d13b457d4c4ca12bbe0f
Garri <cibergarri@hotmail.com>
2020-04-11 16:43:35 +0200

    wip: first commit

---
+++ precache-manifest.6790ef971baaf24b38b2d867fc68c1ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "046775b70c5f54b79903",
+    "url": "/static/css/main.32eccd27.chunk.css"
+  },
+  {
+    "revision": "046775b70c5f54b79903",
+    "url": "/static/js/main.046775b7.chunk.js"
+  },
+  {
+    "revision": "a4c0bd4ecc394c2cc4f0",
+    "url": "/static/css/1.41851d5a.chunk.css"
+  },
+  {
+    "revision": "a4c0bd4ecc394c2cc4f0",
+    "url": "/static/js/1.a4c0bd4e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8774e72fd0efe0495d67dffae939b98f",
+    "url": "/index.html"
+  }
+];
---
+++ push_notifications/scripts/constants.js
@@ -0,0 +1,10 @@
+/* eslint-env browser */
+
+window.gauntface = window.gauntface || {};
+window.gauntface.CONSTANTS = {
+  GCM_API_KEY: 'AIzaSyBBh4ddPa96rQQNxqiq_qQj7sq1JdsNQUQ',
+  APPLICATION_KEYS: {
+    publicKey: 'BBHQYI95slhkQO07k_jVYw-bvV-5SrA2DgoK3Fa70pQSXiDyMWUpGeCauAuGYMc0fVjxkvrwl4RTm7FswWV6Wwc',
+    privateKey: 'IM_m1X1ALqYNfw6JFLkNR3FlCvtbtjOkYLqu0otRgBc',
+  },
+};
