278e30064e9ae1be015c5a398d210879005984d7
Garri <cibergarri@hotmail.com>
2020-04-11 17:31:13 +0200

    fix: routes

--- react_authorquizz/service-worker.js
+++ react_authorquizz/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react_authorquizz/precache-manifest.5897ed249631c3549534d06611fda666.js"
+  "/playground/react_authorquizz/precache-manifest.5897ed249631c3549534d06611fda666.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react_authorquizz/index.html", {
+workbox.routing.registerNavigationRoute("/playground/react_authorquizz/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
